fdbb63507c4ca8d5f3587289fd72c5fa44648ccc
deepaswamy7 <deepa.swamy7@gmail.com>
2017-04-21 20:55:18 -0400

    Update OrderService.java

--- sample/src/main/resources/com/example/services/OrderService.java
+++ sample/src/main/resources/com/example/services/OrderService.java
@@ -1,15 +1,15 @@
-package com.example.service;
+package com.example.services;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.dao.OrdersDao;
 import com.example.model.Orders;
 
 public interface OrderService {
-	
-	@Autowired
-	private OrdersDao ordersDao;
 
 	List<Orders> findAll();
+
+	Orders findOne(int id);
 }
