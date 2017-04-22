309f76564db63630e62ecdbdbd791f57938c47b7
deepaswamy7 <deepa.swamy7@gmail.com>
2017-04-21 20:55:49 -0400

    Update OrderServiceImpl.java

--- sample/src/main/resources/com/example/services/OrderServiceImpl.java
+++ sample/src/main/resources/com/example/services/OrderServiceImpl.java
@@ -1,12 +1,15 @@
-package com.example.service;
+package com.example.services;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.dao.OrdersDao;
 import com.example.model.Orders;
 
+
+@Service
 public class OrderServiceImpl implements OrderService{
 	
 @Autowired
@@ -17,4 +20,8 @@ public List<Orders> findAll(){
 
 return ordersDao.findAll();
 }
+public Orders findOne(int id){
+	return ordersDao.findOne(id);
+}
+
 }
